eb44a763970bbc4ab4dabba8bd712beec3ef913f
SharfanaSaroor <sharfanachmahin@gmail.com>
2024-11-16 14:10:47 +0530

    Update CodeDemo.cpp

--- src/Ch01/01_03/CodeDemo.cpp
+++ src/Ch01/01_03/CodeDemo.cpp
@@ -6,9 +6,14 @@
 #include <string>
 
 int main(){
-    std::string str;
-    std::cin >> str;
-    std::cout << str;
+    int a[4];
+
+    std:cout<< "Enter 5 numbers";
+    for (int i =0; i<5;i++){
+        cin>>a[i];
+    }
+    float avg = (a[0]+a[1]+a[2]+a[3]+a[4])/5
+    std::cout <<" Average: " + avg;
 
     std::cout << std::endl << std::endl;
     return (0);
